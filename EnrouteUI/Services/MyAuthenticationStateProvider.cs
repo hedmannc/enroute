7ed37465bfb7bca1a53f84d88871566bf18c4e9a
Clinton Hedmann <Clintonh@bcicjamaica.com>
2024-05-07 23:00:52 -0500

    Auth fix

--- EnrouteUI/Services/MyAuthenticationStateProvider.cs
+++ EnrouteUI/Services/MyAuthenticationStateProvider.cs
@@ -10,34 +10,8 @@ public class MyAuthenticationStateProvider : AuthenticationStateProvider {
 
     //Anonymous user can has no claims but requires an empty identity
     private ClaimsPrincipal AnonymousUser => new(new ClaimsIdentity(Array.Empty<Claim>()));
-    private ClaimsPrincipal FakedUser {
-        get {
-            var claims = new[] {
-                new Claim(ClaimTypes.Name, "john"),
-                new Claim(ClaimTypes.Role, "user"),
-            };
-            var identity = new ClaimsIdentity(claims, "faked");
-            return new ClaimsPrincipal(identity);
-        }
-    }
-    private ClaimsPrincipal FakedAdmin {
-        get {
-            var claims = new[] {
-                new Claim(ClaimTypes.Name, "john"),
-                new Claim(ClaimTypes.Role, "admin"),
-            };
-            var identity = new ClaimsIdentity(claims, "faked");
-            return new ClaimsPrincipal(identity);
-        }
-    }
-    public void FakedSignIn() {
-        var result = Task.FromResult(new AuthenticationState(FakedUser));
-        NotifyAuthenticationStateChanged(result);
-    }
-    public void FakedAdminSignIn() {
-        var result = Task.FromResult(new AuthenticationState(FakedAdmin));
-        NotifyAuthenticationStateChanged(result);
-    }
+
+
     public void SignOut() {
         var result = Task.FromResult(new AuthenticationState(AnonymousUser));
         NotifyAuthenticationStateChanged(result);
