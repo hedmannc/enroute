e244c70ce38b607b3d269fa90f0e9514db0478a0
Clinton Hedmann <Clintonh@bcicjamaica.com>
2024-05-09 12:56:42 -0500

    Fix adding my location concurrency

--- EnrouteAppLibrary/Models/ErrorDetails.cs
+++ EnrouteAppLibrary/Models/ErrorDetails.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace EnrouteAppLibrary.Models
@@ -12,6 +13,8 @@ namespace EnrouteAppLibrary.Models
             public string title { get; set; }
             public int status { get; set; }
             public string detail { get; set; }
+
+        public JsonObject errors { get; set; } = new JsonObject();
         
 
     }
